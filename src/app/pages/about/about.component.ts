38f40c02961c05c37bae7a6fecdd71b8925e2c8c
DRK90 <kennedy.d0@gmail.com>
2025-02-19 16:24:40 -0500

    update ext script

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,27 +1,36 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Component, OnInit, Renderer2, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent implements OnInit {
+  private scriptLoaded = false; // To prevent duplicate script injections
 
-  constructor(private renderer: Renderer2) {}
+  constructor(private renderer: Renderer2, @Inject(DOCUMENT) private document: Document) {}
 
   ngOnInit(): void {
-    this.loadDrawIoViewer();
+    if (!this.scriptLoaded) {
+      this.loadDrawIoViewer();
+      this.scriptLoaded = true;
+    }
   }
 
   loadDrawIoViewer(): void {
     const script = this.renderer.createElement('script');
     script.src = 'https://app.diagrams.net/js/viewer-static.min.js';
     script.type = 'text/javascript';
+    script.async = true;
+    script.defer = true;
+
     script.onload = () => {
       console.log('Draw.io Viewer Loaded');
     };
-    document.body.appendChild(script);
+
+    // Append to head instead of body to ensure proper script execution
+    this.renderer.appendChild(this.document.head, script);
   }
 }
