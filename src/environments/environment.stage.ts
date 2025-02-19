99eb970c9d11165f9de757c72da771afc06b8276
DRK90 <kennedy.d0@gmail.com>
2025-02-19 18:37:37 -0500

    update to https for backend

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://ec2-100-25-17-242.compute-1.amazonaws.com'  // API URL for stage
+    apiUrl: 'https://api.quicklycook.com'  // API URL for stage
   };
   
