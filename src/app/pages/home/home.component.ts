4b7af8aea1d3afa7210bbde857a913c37068003d
DRK90 <kennedy.d0@gmail.com>
2025-02-26 16:47:36 -0500

    add edit ability

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -27,6 +27,9 @@ interface RecipeReturnDto {
   recipeCreatedDate: string; // ISO date string
   ingredients: { ingredientId: number; ingredientName: string; ingredientQuantity: number; ingredientUnit: string }[];
   recipeSteps: { stepNumber: number; stepDescription: string }[];
+    // Added for edit functionality
+    isEditing?: boolean;
+    editData?: any;
 }
 
 @Component({
@@ -150,4 +153,89 @@ export class HomeComponent implements OnInit {
       }
     );
   }
+
+  // Start editing a recipe
+startEditing(recipe: RecipeReturnDto): void {
+  // Create a deep copy of recipe data for editing
+  recipe.isEditing = true;
+  recipe.editData = JSON.parse(JSON.stringify({
+    recipeName: recipe.recipeName,
+    recipeType: recipe.recipeType,
+    recipeDescription: recipe.recipeDescription,
+    recipePrepTime: recipe.recipePrepTime,
+    recipeCookTime: recipe.recipeCookTime,
+    ingredients: recipe.ingredients || [],
+    recipeSteps: recipe.recipeSteps || [],
+    recipeNotes: recipe.recipeNotes || []
+  }));
+}
+
+// Cancel editing and discard changes
+cancelEditing(recipe: RecipeReturnDto): void {
+  recipe.isEditing = false;
+  recipe.editData = undefined;
+}
+
+// Save edits to the recipe
+saveEdits(recipe: RecipeReturnDto): void {
+  // Apply edits to the recipe
+  recipe.recipeName = recipe.editData.recipeName;
+  recipe.recipeType = recipe.editData.recipeType;
+  recipe.recipeDescription = recipe.editData.recipeDescription;
+  recipe.recipePrepTime = recipe.editData.recipePrepTime;
+  recipe.recipeCookTime = recipe.editData.recipeCookTime;
+  recipe.ingredients = recipe.editData.ingredients;
+  recipe.recipeSteps = recipe.editData.recipeSteps;
+  recipe.recipeNotes = recipe.editData.recipeNotes;
+  
+  // Exit edit mode
+  recipe.isEditing = false;
+  recipe.editData = undefined;
+  
+  // TODO: Call your API to update the recipe
+  // this.updateRecipe(recipe);
+}
+
+// Add a new ingredient
+addIngredient(recipe: RecipeReturnDto): void {
+  recipe.editData.ingredients.push({
+    ingredientId: 0, // Will be assigned by the server
+    ingredientName: '',
+    ingredientQuantity: 1,
+    ingredientUnit: ''
+  });
+}
+
+// Remove an ingredient
+removeIngredient(recipe: RecipeReturnDto, index: number): void {
+  recipe.editData.ingredients.splice(index, 1);
+}
+
+// Add a new step
+addStep(recipe: RecipeReturnDto): void {
+  const nextStepNumber = recipe.editData.recipeSteps.length + 1;
+  recipe.editData.recipeSteps.push({
+    stepNumber: nextStepNumber,
+    stepDescription: ''
+  });
+}
+
+// Remove a step
+removeStep(recipe: RecipeReturnDto, index: number): void {
+  recipe.editData.recipeSteps.splice(index, 1);
+  // Re-number the steps
+  recipe.editData.recipeSteps.forEach((step: { stepNumber: number; stepDescription: string }, i: number) => {
+    step.stepNumber = i + 1;
+  });
+}
+
+// Add a new note
+addNote(recipe: RecipeReturnDto): void {
+  recipe.editData.recipeNotes.push('');
+}
+
+// Remove a note
+removeNote(recipe: RecipeReturnDto, index: number): void {
+  recipe.editData.recipeNotes.splice(index, 1);
+}
 }
